986f5a2c937df16d8eaa45633c3b997151aa1f39
PPz <ppzreboot@qq.com>
2024-07-18 14:48:50 +0800

    perf: algorithm

--- index.js
+++ index.js
@@ -6,32 +6,14 @@ function badep(len, to_d) { // 这是一个 npm 上未注册的名字
   if (!Number.isInteger(to_d)) throw Error('to_d should be an integer')
   if (len <= to_d) throw Error('len <= to_d')
 
-  let opposite = false
-  if (len / to_d < 1.5) { // !!! 不好解释
-    to_d = len - to_d
-    opposite = true
-  }
-
   const result = []
+  const step = len / to_d
+  let count = 0
   let i = 0
-  let remaining = len
-  while (true) {
-    let step = Math.round(remaining / to_d)
-    i += step // 被删除元素下标：步长对应的最后一个元素
-    result.push(i - 1)
-
-    to_d--
-    remaining -= step
-    if (remaining === 0)
-      break
+  while(count < to_d) {
+    result.push(Math.round((i + i+step)/2))
+    i += step
+    count++
   }
-
-  if (!opposite)
-    return result
-
-  const oppo_result = []
-  for (let i=0; i<len; i++)
-    if (!result.includes(i))
-      oppo_result.push(i)
-  return oppo_result
+  return result
 }
--- index.test.js
+++ index.test.js
@@ -1,17 +1,7 @@
 import { test, expect } from 'vitest'
 import { badep } from '.'
 
-test('balancing deleting problem', () => {
-  expect(
-    badep(8, 2)
-  ).toEqual([3, 7])
-  expect(
-    badep(8, 3)
-  ).toEqual([2, 5, 7])
-  expect(
-    badep(8, 5)
-  ).toEqual([1, 3, 4, 6, 7])
-
+test('length', () => {
   expect(
     badep(100, 99).length
   ).toBe(99)
@@ -24,12 +14,118 @@ test('balancing deleting problem', () => {
   expect(
     badep(100001, 303).length
   ).toBe(303)
+  expect(
+    badep(100001, 999).length
+  ).toBe(999)
+  expect(
+    badep(100001, 10001).length
+  ).toBe(10001)
+  expect(
+    badep(100001, 100000).length
+  ).toBe(100000)
+  expect(
+    badep(100001, 1).length
+  ).toBe(1)
+})
 
-  // 加了 opposite 之后，下面的单元测试失效
-  // expect(
-  //   badep(101, 97).at(-1)
-  // ).toBe(100)
-  // expect(
-  //   badep(10001, 1001).at(-1)
-  // ).toBe(10000)
+test('duplicated element', () => {
+  expect(
+    badep(10001, 777).length
+  ).toBe(
+    Array.from(new Set(badep(10001, 777))).length
+  )
+  expect(
+    badep(10001, 999).length
+  ).toBe(
+    Array.from(new Set(badep(10001, 999))).length
+  )
+  expect(
+    badep(10001, 8000).length
+  ).toBe(
+    Array.from(new Set(badep(10001, 8000))).length
+  )
+  expect(
+    badep(10001, 3).length
+  ).toBe(
+    Array.from(new Set(badep(10000, 3))).length
+  )
+  expect(
+    badep(10001, 10000).length
+  ).toBe(
+    Array.from(new Set(badep(10001, 10000))).length
+  )
+})
+
+function is_incr(arr) {
+  for (let i=0; i<arr.length-1; i++)
+    if (arr[i] >= arr[i+1])
+      return false
+  return true
+}
+test('is_incr', () => {
+  expect(is_incr([1,2,3])).toBe(true)
+  expect(is_incr([1,1.1,1.3])).toBe(true)
+  expect(is_incr([1,1.9999999999999,2])).toBe(true)
+  expect(is_incr([1,0.9,1.3])).toBe(false)
+})
+test('increment', () => {
+  expect(
+    is_incr(badep(10001, 10000))
+  ).toBe(true)
+  expect(
+    is_incr(badep(10001, 101))
+  ).toBe(true)
+  expect(
+    is_incr(badep(10001, 99))
+  ).toBe(true)
+  expect(
+    is_incr(badep(10001, 5000))
+  ).toBe(true)
+
+  expect(
+    is_incr(badep(800, 400))
+  ).toBe(true)
+  expect(
+    is_incr(badep(800, 32))
+  ).toBe(true)
+  expect(
+    is_incr(badep(800, 700))
+  ).toBe(true)
+  expect(
+    is_incr(badep(800, 1))
+  ).toBe(true)
+})
+
+test('in range', () => {
+  let result = badep(10001, 100)
+  expect(result.at(0)).gte(0)
+  expect(result.at(-1)).lte(10000)
+
+  result = badep(10001, 10000)
+  expect(result.at(0)).gte(0)
+  expect(result.at(-1)).lte(10000)
+
+  result = badep(10001, 999)
+  expect(result.at(0)).gte(0)
+  expect(result.at(-1)).lte(10000)
+
+  result = badep(10001, 1)
+  expect(result.at(0)).gte(0)
+  expect(result.at(-1)).lte(10000)
+
+  result = badep(10001, 9999)
+  expect(result.at(0)).gte(0)
+  expect(result.at(-1)).lte(10000)
+
+  result = badep(8, 7)
+  expect(result.at(0)).gte(0)
+  expect(result.at(-1)).lte(7)
+
+  result = badep(4, 3)
+  expect(result.at(0)).gte(0)
+  expect(result.at(-1)).lte(3)
+
+  result = badep(6, 2)
+  expect(result.at(0)).gte(0)
+  expect(result.at(-1)).lte(5)
 })
